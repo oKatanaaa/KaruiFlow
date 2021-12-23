67e30867acc4b8cab1b3dc3c09a198a3e283cf4c
oKatanaaa <whitemarsstudios@gmail.com>
2021-12-23 22:15:10 +0400

    fix(Parameter): copying data from the source storage

---
+++ KaruiFlow/KaruiFlow/core/src/Parameter.cpp
@@ -0,0 +1,12 @@
+#include "../headers/Parameter.h"
+
+
+namespace karuiflow {
+	Parameter::Parameter(Tensor* tensor) {
+		m_Data = tensor->m_Data->createSimilar();
+		m_Data->copyFrom(tensor->m_Data);
+
+		m_Specs = TensorSpecs{ tensor->m_Specs.dtype->copy(),  tensor->m_Specs.shape, tensor->m_Specs.device };
+		m_RequiresGrad = true;
+	}
+}
