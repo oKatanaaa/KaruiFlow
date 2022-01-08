e026d565ed77ac180bd6d2e9d5f8921f272b6639
AlexRikka <rikka.gold@mail.ru>
2022-01-09 00:41:50 +0400

    new: added softmax kernel backward

--- KaruiFlow/KaruiFlow/operations/softmax/SoftmaxCUDAKernels.cu
+++ KaruiFlow/KaruiFlow/operations/softmax/SoftmaxCUDAKernels.cu
@@ -43,12 +43,45 @@ namespace karuiflow {
 
 	void SoftmaxCudaKernel::backward(std::vector<Storage*> inputs, std::vector<bool> requiresGrad,
 		Storage* outerGradient, std::vector<Storage*> outputGradients) {
-		float* inputData = (float*)inputs[0]->getData();
-		float* _outerGradient = (float*)outerGradient->getData();
-		float* outputGradient = (float*)outputGradients[0]->getData();
-		size_t nElems = inputs[0]->getSize();
+		if (requiresGrad[0]) {
+			cudnnHandle_t* handle = CudaContextManager::getCudnnHandle();
+			std::vector<int> inputDataShape = inputs[0]->getShape();
+			// For 2D Tensor: n=batch-size, c=channels
+			int n = inputDataShape[0];
+			int c = inputDataShape[1];
+			int h = 1, w = 1;
+			cudnnTensorDescriptor_t inputDesc;
+			cudnnCreateTensorDescriptor(&inputDesc);
+			cudnnSetTensor4dDescriptor(inputDesc,
+				CUDNN_TENSOR_NCHW,
+				CUDNN_DATA_FLOAT,
+				n, c, h, w);
 
-		//int nBlocks = (nElems + m_ThreadsPerBlock - 1) / m_ThreadsPerBlock;
+			cudnnTensorDescriptor_t outputDesc;
+			cudnnCreateTensorDescriptor(&outputDesc);
+			cudnnSetTensor4dDescriptor(outputDesc,
+				CUDNN_TENSOR_NCHW,
+				CUDNN_DATA_FLOAT,
+				n, c, h, w);
+
+			cudnnTensorDescriptor_t gradDesc;
+			cudnnCreateTensorDescriptor(&gradDesc);
+			cudnnSetTensor4dDescriptor(gradDesc,
+				CUDNN_TENSOR_NCHW,
+				CUDNN_DATA_FLOAT,
+				n, c, h, w);
+
+			forward(inputs, outputGradients[0]);
+			float* _outerGradient = (float*)outerGradient->getData();
+			float* outputGradient = (float*)outputGradients[0]->getData();
+			const float alpha = 1, beta = 0;
+	
+			cudnnSoftmaxBackward(*handle,
+				CUDNN_SOFTMAX_FAST, CUDNN_SOFTMAX_MODE_CHANNEL,
+				&alpha, inputDesc, outputGradient,
+				gradDesc, _outerGradient,
+				&beta, outputDesc, outputGradient);
+		}
+		
 	}
-
 }
