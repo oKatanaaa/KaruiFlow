fix(numpy mul kernel): wrong dimensionality reduction in gradients

KaruiFlowCython/karuiflow/core/numpy_kernels/mul.py
@@ -27,17 +27,19 @@ class MulKernel(PythonKernel):
 
         if A_requires_grad:
             dims_to_reduce = []
-            for i, dim in enumerate(B.shape):
-                if dim == 1:
-                    dims_to_reduce.append(i)
-            grad = np.sum(B, axis=tuple(dims_to_reduce), keepdims=True)
-            np.copyto(A_grad, grad)
-
-        if B_requires_grad:
-            dims_to_reduce = []
             for i, dim in enumerate(A.shape):
                 if dim == 1:
                     dims_to_reduce.append(i)
+            outerGradient_A = np.sum(outerGradient, axis=tuple(dims_to_reduce), keepdims=True)
+            grad = np.sum(B, axis=tuple(dims_to_reduce), keepdims=True)
+            np.copyto(A_grad, grad * outerGradient_A)
+
+        if B_requires_grad:
+            dims_to_reduce = []
+            for i, dim in enumerate(B.shape):
+                if dim == 1:
+                    dims_to_reduce.append(i)
+            outerGradient_B = np.sum(outerGradient, axis=tuple(dims_to_reduce), keepdims=True)
             grad = np.sum(A, axis=tuple(dims_to_reduce), keepdims=True)
-            np.copyto(B_grad, grad)
+            np.copyto(B_grad, grad * outerGradient_B)
         logging.debug(f'{self.__class__.__name__}.backward successful.')